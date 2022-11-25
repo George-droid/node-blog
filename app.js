9b8463e8e2d488d49d5cf17297802847d603fbf6
Georgedroid <wisgeorge.wg@gail.com>
2022-11-25 16:32:05 +0100

    Modified multiple files

--- app.js
+++ app.js
@@ -1,37 +1,55 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const { result } = require('lodash');
+const blogRoutes = require('./routes/blogRoutes')
 
 //express app
 const app = express();
 
+// connect to mongodb
+const dbURI = 'mongodb+srv://layer3:Layer3.@nodeblog.3uj6rnf.mongodb.net/note-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
+
+
 //Register view engine
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
-//listen for request
-app.listen(3000);
 
-// middlewares and statics
+ //middlewares and statics
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
+/*mongoose and mongo sanbox routes
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'new blog 2',
+        snippet: 'about my new blog',
+        body: 'more about my new blog'
+    });
+
+    blog.save()
+        .then((result) => {
+            res.send(result)    
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+});
+*/
+
+
+
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-      ];
-    res.render('index', {title: 'Home', blogs });
+    res.redirect('/blogs');
 });
 
-app.get('/about', (req, res) => {
-
-    res.render('about', {title: 'About'});
-});
-
-app.get('/blogs/create' , (req, res) => {
-    res.render('create', {title: 'Create a new Blog'});
-
-})
+//blog routes
+app.use(blogRoutes);
 
 //404
 app.use((req, res) => {
